e496a340b64cd84db6810efcedd3f8a9f1683050
deisler134 <microgalaxy134@gmail.com>
2019-02-11 15:46:29 -0500

    add padding image

--- cocoapi/PythonAPI/mask_processing.py
+++ cocoapi/PythonAPI/mask_processing.py
@@ -12,8 +12,11 @@ import os
 
 debug_mode = True
 
-imagepath = '../Data/project/humanparsing/JPEGImages/'
-maskpath = '../Data/project/humanparsing/SegmentationClassAug/'
+# imagepath = '/media/deisler/Data/project/humanparsing/JPEGImages/'
+# maskpath = '/media/deisler/Data/project/humanparsing/SegmentationClassAug/'
+
+imagepath = '/media/deisler/Data/project/LIP/single/LIP/TrainVal_images/TrainVal_images/train_images'
+maskpath = '/media/deisler/Data/project/LIP/single/LIP/TrainVal_parsing_annotations/TrainVal_parsing_annotations/train_segmentations'
 
 def get_fileslist( imagepath = imagepath ):
     print(os.path.join(imagepath, '*'))
@@ -23,13 +26,21 @@ def get_imagename( imagepath ):
     
     return imagepath.split('/')[-1]
 
+def check_image(src):
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image!')
+        return False
+    else:
+        return True
+
 def get_colormask( image ):
     if len(image.shape) == 3:
         mask = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     else:
         mask = image.copy()
 #     thresh = cv.adaptiveThreshold(mask,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
-    retval, thresh = cv.threshold(mask,1, 255,cv.THRESH_BINARY)
+    retval, thresh = cv.threshold(mask,0.1, 255,cv.THRESH_BINARY)
     
     _, contours, hierarchy = cv.findContours(
        thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE
@@ -50,7 +61,7 @@ def mask_on_image(imagepath, maskpath):
     img = cv.imread(imagepath)
     mask = cv.imread(maskpath)
     
-    if (img.size != 0) and (mask.size != 0):
+    if check_image( img ) and check_image( mask ):
         mask = get_colormask( mask )
         
     maskimage = np.where(mask, img*0.3 + mask*0.7, img).astype(np.uint8)
@@ -76,11 +87,21 @@ def save_maskon_image(imagepath = imagepath, maskpath = maskpath):
         print(imgpath,mkpath)
         
         maskimage = mask_on_image(imgpath, mkpath)
-        blendimgpath = os.path.join(imagepath, imagename[:-4]+'_mask.jpg')
+        blendimgpath = os.path.join('/media/deisler/Data/project/LIP/single/LIP/TrainVal_images/TrainVal_images/mask', imagename[:-4]+'_mask.jpg')
 
         cv.imwrite(blendimgpath, maskimage)
            
-        
-save_maskon_image(imagepath,maskpath)            
+def show_mask_image(imagepath = imagepath, maskpath = maskpath):
+    
+    imgpath = os.path.join(imagepath, '4975_437954.jpg') 
+    mkpath = os.path.join(maskpath, '4975_437954.png')
+    print(imgpath,mkpath)
+    maskimage = mask_on_image(imgpath, mkpath)
+    
+# show_mask_image(imagepath,maskpath)
+ 
+save_maskon_image(imagepath,maskpath)      
+
+      
             
         
